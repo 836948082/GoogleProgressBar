4a5e83b9671c81b34f58a688ac5731d55a9ddad6
836948082 <836948082@qq.com>
2016-07-20 16:55:39 +0800

    GoogleProgressBar

---
+++ README.md
@@ -0,0 +1,15 @@
+GoogleProgressBar
+===============
+
+Android library to display different kind of google related animations for the progressBar.
+
+The [demo app][1] can be found in the play store. 
+
+These animations have been finished so far:
+  
+![FoldingCirclesProgressBar][1] ![GoogleMusicDicesDrawable][2] ![NexusRotationCross.gif][3] ![NexusRotationCross.gif][4]
+
+[1]: https://raw.githubusercontent.com/jpardogo/GoogleProgressBar/master/art/GoogleProgressBar.gif
+[2]: https://raw.githubusercontent.com/jpardogo/GoogleProgressBar/dev/art/GoogleDices.gif
+[3]: https://raw.githubusercontent.com/jpardogo/GoogleProgressBar/master/art/NexusRotationCross.gif
+[4]: https://raw.githubusercontent.com/MewX/google-progress-bar/gpb-chrome/art/ChromeFloatingCircles.gif
---
+++ app/src/main/java/com/runtai/googleprogressbar/SettingsActivity.java
@@ -0,0 +1,32 @@
+package com.runtai.googleprogressbar;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    public static Intent newInstance(Context context){
+        return new Intent(context, SettingsActivity.class);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
